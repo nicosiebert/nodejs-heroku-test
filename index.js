b00086fd84cc7f4083fab263b85a5eea4ca01c54
slider <nicosiebert84@gmail.com>
2021-11-21 20:30:33 -0300

    firt commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.json())
+
+app.listen(process.env.PORT || 3000, (req, res)=>{
+    if(process.env.PORT){
+        console.log("servidor escuchando en el puerto" + process.env.PORT)
+    }else{
+        console.log("servidor escuchando en el puerto " +3000);
+    }
+    
+    
+})
+
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Hola</h1>");
+})
