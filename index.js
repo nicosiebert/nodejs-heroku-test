cd4d775c227e948e0be08a00e2176e0c84c03155
Nicolás Siebert <87218939+nicosiebert@users.noreply.github.com>
2021-11-22 04:57:41 -0300

    aa

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//setting
+app.set("port", process.env.HOST || 80);
+
+//middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("view engine", "ejs");
 
 //public
 app.use(express.static("./public"));
 //router
 app.use(require("./src/router/router"));
 //server
-app.listen(process.env.PORT || 3000, (req, res) => {
-  if (process.env.PORT) {
-    console.log("servidor escuchando en el puerto" + process.env.PORT);
-  } else {
-    console.log("servidor escuchando en el puerto " + 3000);
-  }
+app.listen(app.get("port"), (req, res) => {
+  console.log("servidor escuchando en el puerto" + app.get("port"));
 });
--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index.ejs");
+});
